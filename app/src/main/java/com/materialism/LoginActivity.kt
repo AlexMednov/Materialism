a74b096e5ebfc49e4007bf9bb3d42eed37ae508c
Alex Schuepbach <alex.schupbach@student.nhlstenden.com>
2024-05-13 15:16:15 +0200

    Renamed login page files to login

--- app/src/main/java/com/materialism/LoginActivity.kt
+++ app/src/main/java/com/materialism/LoginActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
 
         val loginButton = findViewById<Button>(R.id.loginButton)
 

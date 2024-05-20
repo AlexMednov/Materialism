05071ac86e118ed9be33ceb9fbdf5f8c5a332f93
unknown <sebastian.guntzel@student.nhlstenden.com>
2024-05-20 16:26:28 +0200

    Added camera code, does not function correctly

--- app/src/main/java/com/materialism/MainPageActivity.kt
+++ app/src/main/java/com/materialism/MainPageActivity.kt
@@ -10,37 +10,44 @@ import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 
 class MainPageActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main_page_activity)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_page_activity)
 
-    // Registers a photo picker activity launcher in single-select mode.
-    val pickMedia =
-        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-          val text = findViewById<View>(R.id.fileUri) as TextView
-          // Callback is invoked after the user selects a media item or closes the
-          // photo picker.
-          if (uri != null) {
-            text.text = uri.toString()
-          } else {
-            text.text = "No media selected"
-          }
-        }
+        // Registers a photo picker activity launcher in single-select mode.
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                val text = findViewById<View>(R.id.fileUri) as TextView
+                // Callback is invoked after the user selects a media item or closes the
+                // photo picker.
+                if (uri != null) {
+                    text.text = uri.toString()
+                } else {
+                    text.text = "No media selected"
+                }
+            }
 
-    val uploadFilesButton = findViewById<Button>(R.id.uploadFiles)
-    val backButton = findViewById<Button>(R.id.back)
+        val uploadFilesButton = findViewById<Button>(R.id.uploadFiles)
+//        val accessCameraButton = findViewById<Button>(R.id.accessCamera)
+        val backButton = findViewById<Button>(R.id.back)
 
-    uploadFilesButton.setOnClickListener(
-        View.OnClickListener {
-          pickMedia.launch(
-              PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        })
+        uploadFilesButton.setOnClickListener(
+            View.OnClickListener {
+                pickMedia.launch(
+                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+            })
 
-    backButton.setOnClickListener(
-        View.OnClickListener {
-          val intent = Intent(this, LoginActivity::class.java)
+        backButton.setOnClickListener(
+            View.OnClickListener {
+                val intent = Intent(this, LoginActivity::class.java)
 
-          startActivity(intent)
-        })
-  }
+                startActivity(intent)
+            })
+
+//        accessCameraButton.setOnClickListener(
+//            View.OnClickListener {
+//
+//            }
+//        )
+    }
 }

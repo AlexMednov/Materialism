abc3053fa20443d1570ab81a18851316abca3bac
Alex Mednov <114412787+AlexMednov@users.noreply.github.com>
2024-05-30 14:31:08 +0200

    Created basis json for db as well as data classes, going to make more entries into json and put it into firebase

--- app/src/main/java/com/materialism/firebaseDatabase/data/DataClass.kt
+++ app/src/main/java/com/materialism/firebaseDatabase/data/DataClass.kt
@@ -1,63 +1,15 @@
 package com.materialism.firebaseDatabase.data
 
-data class User(
-    val id: String = "",
-    val name: String = "",
-    val emailAddress: String? = null,
-    val isRegistered: Boolean = false,
-    val score: Int = 0,
-    val karma: Int = 0
-)
+import java.util.Date
 
-data class Item(
-    val id: String = "",
-    val name: String = "",
-    val imageURI: String = "",
-    val description: String? = null,
-    val location: String? = null,
-    val isPublic: Boolean = false,
-    val isLoaned: Boolean = false,
-    val dateTimeAdded: String = "",
-    val dateTimeModified: String = "",
-    val userId: String = "",
-    val categoryId: String = "",
-    val subcategoryId: String? = null
-)
+data class QuestItem(val id: Int = 0, val name: String = "Default name", val categoryId: Int = 0)
 
-data class Subcategory(
-    val id: String = "",
-    val name: String = "",
-    val description: String? = null
-)
+data class Quest(val id: Int = 0, val type: String = "Default quest", val weight: Int = 0, val categoryId: Int = 0)
 
-data class Category(
-    val id: String = "",
-    val name: String = "",
-    val description: String? = null,
-    val isDefault: Boolean = false,
-    val subcategories: Map<String, Subcategory> = emptyMap()
-)
+data class User(val id: Int = 0, val name: String = "User Userovich", val email: String? = null, val hashedPassword: String? = null,val isRegistered: Boolean = false, val score: Int = 0, val karma: Int = 0)
 
-data class Quest(
-    val id: String = "",
-    val type: Int = 0,
-    val weight: Int = 0,
-    val categoryId: String = ""
-)
+data class Category(val id: Int = 0, val name: String = "Default category", val description: String? = null, val isDefault: Boolean = false)
 
-data class QuestItem(
-    val id: String = "",
-    val name: String = "",
-    val categoryId: String = ""
-)
+data class SubCategory(val id: Int = 0, val name: String = "Default name", val description: String? = null, val categoryId: Int = 0)
 
-data class FirebaseData(
-    val users: Map<String, User> = emptyMap(),
-    val items: Map<String, Item> = emptyMap(),
-    val categories: Map<String, Category> = emptyMap(),
-    val quests: Map<String, Quest> = emptyMap(),
-    val questItems: Map<String, QuestItem> = emptyMap()
-)
-
-//Need to go through both json and dataclass more precisely, for they appear to be a bit goofy
-
+data class Item(val id: Int = 0, val name: String = "Default name", val imageURI: String = "defaultImage.png", val description: String? = null, val location: String? = null, val isPublic: Boolean = false, val isLoaned: Boolean = false, val dateAdded: Date, val dateModified: Date? = null, val userId: Int = 0, val categoryId: Int = 0, val subcategoryId: Int = 0)

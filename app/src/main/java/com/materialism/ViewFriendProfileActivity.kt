38f37575af75b86e2aec5d932e1138455bc83505
Nikolay Minins <nikolay.minins@student.nhlstenden.com>
2024-06-18 19:05:15 +0200

    Fixed error when clicking on the friend box

--- app/src/main/java/com/materialism/ViewFriendProfileActivity.kt
+++ app/src/main/java/com/materialism/ViewFriendProfileActivity.kt
@@ -3,14 +3,9 @@ package com.materialism
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
 import com.materialism.utils.DrawerUtils
 
 class ViewFriendProfileActivity : AppCompatActivity() {
-
-  private lateinit var drawerLayout: DrawerLayout
-  private lateinit var navigationView: NavigationView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
